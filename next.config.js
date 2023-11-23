4b57872351a913d397d44deddbe496ce15cdacbf
finlaywu <finlaywu@tencent.com>
2023-11-23 19:38:10 +0800

    feat:配置更新

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-let path = require("path");
+
 const nextConfig = {
   basePath: process.env.NEXT_BASE_PATH,
-  webpack: (config) => {
-    config.resolve.alias["@styles"] = path.join(__dirname, "src", "assets");
-    return config;
-  },
 };
 
 module.exports = nextConfig;
