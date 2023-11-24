550c29d442fdddf0b44549c66319d93496111d81
finlaywu <finlaywu@tencent.com>
2023-11-24 09:53:36 +0800

    feat: 添加delploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,17 @@
 
+echo 安装nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 source ~/.bashrc
+
+
 
 nvm install 20
  
 use 20
 
+echo $(node -v)
+
+npm i
 
 npm run build
